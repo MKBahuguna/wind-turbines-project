bbf27ac1f05dd257e3894edf42040a8e7f3575cd
Dmitry Foshin <foshindv@gmail.com>
2025-01-12 21:29:34 +0300

    add csv data

--- src/pipeline/config.py
+++ src/pipeline/config.py
@@ -1,4 +1,5 @@
-raw_data_path = "s3a://databricks-workspace-stack-7eec3-bucket/unity-catalog/data/*.csv"
+#raw_data_path = "s3a://databricks-workspace-stack-7eec3-bucket/unity-catalog/data/*.csv"
+raw_data_path = "/Workspace/Shared/data/*.csv"
 cleaned_table_name = 'dmfos_trial.raw.cleaned'
 statistics_table_name = 'dmfos_trial.raw.statistics'
 anomalies_table_name = 'dmfos_trial.raw.anomalies'
